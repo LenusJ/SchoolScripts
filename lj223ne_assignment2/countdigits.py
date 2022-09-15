88d11f0aa2994a25ff5a8ea08f0726ca039aa56a
LenusJ <lj223ne@student.lnu.se>
2022-09-15 18:48:50 +0200

    Finished but not commented

--- lj223ne_assignment2/countdigits.py
+++ lj223ne_assignment2/countdigits.py
@@ -1,6 +1,22 @@
+
 numIn = int(input("Enter a large positive integer: "))
 
-numList = [str(numIn)]
+odd = 0
+even = 0
+zero = 0
 
-print(numList)
+if numIn > 0:
+    x = [int(num) for num in str(numIn)]
 
+    for i in x:
+        if i == 0:
+            zero += 1
+        elif i % 2:
+            odd += 1
+        elif i % 2 == 0:
+            even += 1
+    print("Zeros:", zero)
+    print("Odd:", odd)
+    print("Even:", even)
+else:
+    print("Error: Not a positive int!")
