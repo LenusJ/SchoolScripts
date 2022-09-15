4382a6f7c6be114ee691c70a8b69264739db417d
LenusJ <lj223ne@student.lnu.se>
2022-09-15 17:46:33 +0200

    Completed

---
+++ lj223ne_assignment2/random_numbers.py
@@ -0,0 +1,23 @@
+from random import randint
+
+amount = int(input("Enter number of integers to be generated: "))
+minVal = 0
+maxVal = 0
+numSum = 0
+num = 0
+
+print("\nGenerated values:", end=" ")
+for i in range(0, amount):
+    x = randint(1, 100)
+    print(x, end=" ")
+    if i == 0:
+        minVal = x
+        maxVal = x
+    if x > maxVal:
+        maxVal = x
+    if x < minVal:
+        minVal = x
+    numSum += x
+
+
+print(f"\nAverage, min, and max are {numSum/amount}, {minVal}, {maxVal}")
