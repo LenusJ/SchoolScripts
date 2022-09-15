f7dc08a61dd93b7be8107baf216823fd1c0b8840
LenusJ <lj223ne@student.lnu.se>
2022-09-15 12:52:52 +0200

    File has been completed as of now

--- lj223ne_assignment2/roll_the_die.py
+++ lj223ne_assignment2/roll_the_die.py
@@ -1,38 +1,47 @@
 from random import randint
 
 
-def dicecheck(x, one, two, three, four, five, six):
-    if x == 1:
-        one += 1
-        return one
-    elif x == 2:
-        two += 1
-        return two
-    elif x == 3:
-        three += 1
-        return three
-    elif x == 4:
-        four += 1
-        return four
-    elif x == 5:
-        five += 1
-        return five
-    elif x == 6:
-        six += 1
-        return six
+""" Calculates the difference between biggest and smallest number
+then returns the value as a percent """
+
+
+def diffCalc(biggest, smallest):
+    decNum = (biggest - smallest) / biggest
+    percent = decNum * 100
+    return percent
 
 
 roll = 0
-
 one = 0
 two = 0
 three = 0
 four = 0
 five = 0
 six = 0
+rollAmount = 10
 
-for i in range(0, 11):
-    roll = randint(1, 6)
-    dicecheck(roll, one, two, three, four, five, six)
-
-print(one, two, three, four, five, six)
+""" Checks the average 20 times with each time the dice is being rolled 2 as
+many as the last time
+then store the number of times a certain face has been rolled in variables
+after that check which is the biggest and smallest and send it to the function
+diffCalc lastly print the results. """
+for j in range(0, 21):
+    for i in range(0, rollAmount + 1):
+        roll = randint(1, 6)
+        if roll == 1:
+            one += 1
+        elif roll == 2:
+            two += 1
+        elif roll == 3:
+            three += 1
+        elif roll == 4:
+            four += 1
+        elif roll == 5:
+            five += 1
+        elif roll == 6:
+            six += 1
+    bigNum = max(one, two, three, four, five, six)
+    smallNum = min(one, two, three, four, five, six)
+    perc = diffCalc(bigNum, smallNum)
+    print(f"For {rollAmount} rolls, the difference is {round(perc, 2)}%")
+    rollAmount *= 2
