cb1fbce9765c55863d04dc6d15c3e1aa2cd315e4
LenusJ <lj223ne@student.lnu.se>
2022-09-17 13:47:34 +0200

    Completed

--- lj223ne_assignment2/birthday_candles.py
+++ lj223ne_assignment2/birthday_candles.py
@@ -2,20 +2,18 @@
 candles = 0
 birthday = 0
 boxes = 0
+boxesNeeded = 0
 
 for i in range(0, 100):
     birthday += 1
-    boxesNeeded = (birthday // 24) + 1
-    if birthday == 100:
-        boxesNeeded -= 1
-    if candles < birthday:
-        boxes += boxesNeeded
-        candles += 24 * boxesNeeded
-        if birthday < 15 or birthday > 94:
-            print(f"Before birthday {birthday}, buy {boxesNeeded} box(es)")
-        elif birthday == 16:
-            print("\n ...\n")
-
+    while candles < birthday:
+        boxesNeeded += 1
+        candles += 24
+    boxes += boxesNeeded
+    if boxesNeeded > 0:
+        print(f"Before birthday {birthday}, buy {boxesNeeded} box(es)")
+        boxesNeeded = 0
     candles -= birthday
+
 
 print(f"Total number of boxes: {boxes}, Remaining candles: {candles}")
